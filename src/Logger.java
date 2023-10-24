5ea6fc5d296a79bcee37d2cda75c6f446ef15ec7
joshfine2003 <joshfine2003@gmail.com>
2023-10-24 16:41:48 -0400

    Made Logger Static and added MessageHandler

--- src/Logger.java
+++ src/Logger.java
@@ -4,27 +4,19 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;  
 
 public class Logger {
-    private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"); 
-    private String filePath;
-    private int peerID;
-
-    public Logger(int peerID) {
-        this.peerID = peerID;
-        
-        this.filePath = "../logs/log_peer_" + String.valueOf(peerID) + ".log";
-    }
+    private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"); 
 
     // File I/O adapted from w3schools
     // https://www.w3schools.com/java/java_files_create.asp
-    private void writeLog(String message) {
+    private static void writeLog(int peer1, String message) {
         try {
-            FileWriter logger = new FileWriter(this.filePath);
+            FileWriter logger = new FileWriter("../logs/log_peer_" + String.valueOf(peer1) + ".log");
             LocalDateTime now = LocalDateTime.now();  
 
-            logger.write(dtf.format(now) + ": Peer " + peerID + " " + message);
-
+            logger.write(dtf.format(now) + ": Peer " + peer1 + " " + message);
             logger.close();
-            System.out.println("Successfully logged to " + filePath);
+
+            System.out.println("Successfully logged to ../logs/log_peer_" + String.valueOf(peer1) + ".log");
             
         } catch (IOException e) {
             System.out.println("An error occurred.");
@@ -32,17 +24,17 @@ public class Logger {
         }
     }
 
-    public void logTcpConnectionTo(int neighborID) {
+    public static void logTcpConnectionTo(int peerID, int neighborID) {
         String message = "makes a connection to Peer " + neighborID + ".";
-        writeLog(message);
+        writeLog(peerID, message);
     }
 
-    public void logTcpConnectionFrom(int neighborID) {
+    public static void logTcpConnectionFrom(int peerID, int neighborID) {
         String message = "is connected from Peer " + neighborID + ".";
-        writeLog(message);
+        writeLog(peerID, message);
     }
 
-    public void logChangePreferredNeighbors(int[] preferredNeighbors) {
+    public static void logChangePreferredNeighbors(int peerID, int[] preferredNeighbors) {
 
         StringBuilder stringBuilder = new StringBuilder();
 
@@ -58,46 +50,46 @@ public class Logger {
         String neighborsStr = stringBuilder.toString();
         
         String message = "has the preferred neighbors " + neighborsStr;
-        writeLog(message);
+        writeLog(peerID, message);
     }
 
-    public void logChangeOptimisticallyUnchocked(int neighborID) {
+    public static void logChangeOptimisticallyUnchocked(int peerID, int neighborID) {
         String message = "has the optimistically unchoked neighbor [" + neighborID + "].";
-        writeLog(message);
+        writeLog(peerID, message);
     }
 
-    public void logUnchokedBy(int neighborID) {
+    public static void logUnchokedBy(int peerID, int neighborID) {
         String message = "is unchoked by [" + neighborID + "].";
-        writeLog(message);
+        writeLog(peerID, message);
     }
 
-    public void logChokedBy(int neighborID) {
+    public static void logChokedBy(int peerID, int neighborID) {
         String message = "is choked by [" + neighborID + "].";
-        writeLog(message);
+        writeLog(peerID, message);
     }
 
-    public void logReceivedHave(int neighborID, int pieceIndex) {
+    public static void logReceivedHave(int peerID, int neighborID, int pieceIndex) {
         String message = "received the 'have' message from" + neighborID + "for the piece " + pieceIndex + ".";
-        writeLog(message);
+        writeLog(peerID, message);
     }
 
-    public void logReceivedInterested(int neighborID) {
+    public static void logReceivedInterested(int peerID, int neighborID) {
         String message = "received the 'interested' message from " + neighborID + ".";
-        writeLog(message);
+        writeLog(peerID, message);
     }
 
-    public void logReceivedNotInterested(int neighborID) {
-        String message = "received the ‘not interested’ message from " + neighborID + ".";
-        writeLog(message);
+    public static void logReceivedNotInterested(int peerID, int neighborID) {
+        String message = "received the 'not interested' message from " + neighborID + ".";
+        writeLog(peerID, message);
     }
 
-    public void logDownloadedPiece(int neighborID, int pieceIndex, int numPieces) {
+    public static void logDownloadedPiece(int peerID, int neighborID, int pieceIndex, int numPieces) {
         String message = "has downloaded the piece " +  pieceIndex + "from " + neighborID + ". Now the number of pieces it has is " + numPieces + ".";
-        writeLog(message);
+        writeLog(peerID, message);
     }
 
-    public void logDownloadCompleted() {
+    public static void logDownloadCompleted(int peerID) {
         String message = "has downloaded the complete file.";
-        writeLog(message);
+        writeLog(peerID, message);
     }
 }
--- src/MessageHandler.java
+++ src/MessageHandler.java
@@ -1,3 +1,46 @@
 public class MessageHandler {
-    
+    public static void handle(Message message, Peer self, int peerID){
+        byte[] payload = message.getPayload();
+        switch(message.getType()){
+            case 0: //Choke
+                Logger.logChokedBy(self.peerID, peerID);
+                break;
+            case 1: //Unchoke
+                Logger.logUnchokedBy(self.peerID, peerID);
+                break;
+            case 2: //Interested
+                Logger.logReceivedInterested(self.peerID, peerID);
+                break;
+            case 3: //Not Interested
+                Logger.logReceivedNotInterested(self.peerID, peerID);
+                break;
+            case 4: //Have
+                byte[] indexH = {payload[0], payload[1], payload[2], payload[3]};
+                int pieceIndexH = Helper.byteArrayToInt(indexH);
+                Logger.logReceivedHave(self.peerID, peerID, pieceIndexH);
+                break;
+            case 5: //Bitfield
+
+                break;
+            case 6: //Request
+
+                break;
+            case 7: //Piece
+                byte[] indexP = {payload[0], payload[1], payload[2], payload[3]};
+                int pieceIndexP = Helper.byteArrayToInt(indexP);
+                int pieceCount = 0;
+                for(boolean hasPiece : self.bitfield){
+                    if(hasPiece){
+                        pieceCount++;
+                    }
+                }
+                Logger.logDownloadedPiece(self.peerID, peerID, pieceIndexP, pieceCount);
+                if(pieceCount == Math.ceil((double)ConfigHandler.commonVars.fileSize/ConfigHandler.commonVars.pieceSize)){
+                    Logger.logDownloadCompleted(self.peerID);
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid type.");
+        }
+    }
 }
