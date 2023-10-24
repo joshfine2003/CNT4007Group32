dd600945f57ec9c56dc2d43b17d3a14a55fc7142
lach9859 <73550815+lach9859@users.noreply.github.com>
2023-10-24 17:07:09 -0400

    Added comments to logger.java, edited functionality append to files

--- src/Logger.java
+++ src/Logger.java
@@ -4,14 +4,16 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;  
 
 public class Logger {
-    private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"); 
+    // Specify date time format for log files
+    private static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 
     // File I/O adapted from w3schools
     // https://www.w3schools.com/java/java_files_create.asp
+    // Write message out to logfile
     private static void writeLog(int peer1, String message) {
         try {
-            FileWriter logger = new FileWriter("../logs/log_peer_" + String.valueOf(peer1) + ".log");
-            LocalDateTime now = LocalDateTime.now();  
+            FileWriter logger = new FileWriter("../logs/log_peer_" + String.valueOf(peer1) + ".log", true); // Includes boolean to append instead of overwrite existing file
+            LocalDateTime now = LocalDateTime.now();
 
             logger.write(dtf.format(now) + ": Peer " + peer1 + " " + message);
             logger.close();
@@ -24,16 +26,19 @@ public class Logger {
         }
     }
 
+    // Log when peer makes a TCP connection to other peer
     public static void logTcpConnectionTo(int peerID, int neighborID) {
         String message = "makes a connection to Peer " + neighborID + ".";
         writeLog(peerID, message);
     }
 
+    // Log when peer is connected from another peer
     public static void logTcpConnectionFrom(int peerID, int neighborID) {
         String message = "is connected from Peer " + neighborID + ".";
         writeLog(peerID, message);
     }
 
+    // Log when peer changes its preferred neighbors
     public static void logChangePreferredNeighbors(int peerID, int[] preferredNeighbors) {
 
         StringBuilder stringBuilder = new StringBuilder();
@@ -53,41 +58,49 @@ public class Logger {
         writeLog(peerID, message);
     }
 
+    // Log when peer changes its optimistically unchoked neighbor
     public static void logChangeOptimisticallyUnchocked(int peerID, int neighborID) {
         String message = "has the optimistically unchoked neighbor [" + neighborID + "].";
         writeLog(peerID, message);
     }
 
+    // Log when peer is unchoked by a neighbor
     public static void logUnchokedBy(int peerID, int neighborID) {
         String message = "is unchoked by [" + neighborID + "].";
         writeLog(peerID, message);
     }
 
+    // Log when peer is choked by a neighbor
     public static void logChokedBy(int peerID, int neighborID) {
         String message = "is choked by [" + neighborID + "].";
         writeLog(peerID, message);
     }
 
+    // Log when peer receives a 'have' message
     public static void logReceivedHave(int peerID, int neighborID, int pieceIndex) {
         String message = "received the 'have' message from" + neighborID + "for the piece " + pieceIndex + ".";
         writeLog(peerID, message);
     }
 
+    // Log when peer receives an 'interested' message
     public static void logReceivedInterested(int peerID, int neighborID) {
         String message = "received the 'interested' message from " + neighborID + ".";
         writeLog(peerID, message);
     }
 
+    // Log when peer receives a 'not interested' message
     public static void logReceivedNotInterested(int peerID, int neighborID) {
         String message = "received the 'not interested' message from " + neighborID + ".";
         writeLog(peerID, message);
     }
 
+    // Log when peer finishes downloading a piece
     public static void logDownloadedPiece(int peerID, int neighborID, int pieceIndex, int numPieces) {
         String message = "has downloaded the piece " +  pieceIndex + "from " + neighborID + ". Now the number of pieces it has is " + numPieces + ".";
         writeLog(peerID, message);
     }
 
+    // Log when peer downloads complete file
     public static void logDownloadCompleted(int peerID) {
         String message = "has downloaded the complete file.";
         writeLog(peerID, message);
